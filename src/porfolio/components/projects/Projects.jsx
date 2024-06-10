729cec2b27e9142d15311f444fc62994140cf28e
nn-fiha <jucse28.361@gmail.com>
2024-06-10 10:02:19 +0600

    updated projects section

--- src/porfolio/components/projects/Projects.jsx
+++ src/porfolio/components/projects/Projects.jsx
@@ -8,37 +8,37 @@ const Projects = () => {
     <span className='text-xs text-gray-500'>My existing projects</span>
     <div className="grid grid-cols-2 md:flex md:flex-wrap md:justify-center gap-10 mt-10 md:px-20">
         <div className="w-auto md:w-72  rounded-lg shadow-lg px-4 py-4 border">
-         <img src={process.env.PUBLIC_URL + '/images/OBE2.png'} alt="" className='rounded-lg h-60'/>
+         <img src={process.env.PUBLIC_URL + '/images/OBE.png'} alt="" className='rounded-lg w-full h-36'/>
          <h2 className="text-sm font-medium text-left mt-2">OBE Based Result Management System</h2>
          <a href="https://github.com/whomping-willow/OBE"><h2 className='text-left text-xs text-gray-500 mt-1 '>Git Link -></h2></a>
         </div>
 
-        <div className="w-auto md:w-72  rounded-lg shadow-lg px-4 py-4 border">
-         <img src={process.env.PUBLIC_URL + '/images/kontaNiben.png'} alt="" className='rounded-lg'/>
+        <div className="w-auto md:w-72 rounded-lg shadow-lg px-4 py-4 border">
+         <img src={process.env.PUBLIC_URL + '/images/kontaNiben.png'} alt="" className='rounded-lg w-full  h-36'/>
          <h2 className="text-sm font-medium text-left mt-2">Konta Niben?</h2>
          <a href="https://github.com/SQA-PROJECT-1/Konta-Niben"><h2 className='text-left text-xs text-gray-500 mt-1 '>Git Link -></h2></a>
         </div>
 
         <div className="w-auto md:w-72  rounded-lg shadow-lg px-4 py-4 border">
-         <img src={process.env.PUBLIC_URL + '/images/cyber.jpg'} alt="" className='rounded-lg h-60'/>
+         <img src={process.env.PUBLIC_URL + '/images/cyber.jpg'} alt="" className='rounded-lg'/>
          <h2 className="text-sm font-medium text-left mt-2">Cyber Bullying Detection using Machine Learning</h2>
          <a href="https://github.com/SM2142/Cyber-Bullying-Detection-on-Scocial-Media-Plaform-Using-Machine-Learning"><h2 className='text-left text-xs text-gray-500 mt-1 '>Git Link -></h2></a>
         </div>
 
         <div className="w-auto md:w-72  rounded-lg overflow-hidden shadow-lg px-4 py-4 border">
-         <img src={process.env.PUBLIC_URL + '/images/kontaNiben.png'} alt="" className='rounded-lg'/>
+         <img src={process.env.PUBLIC_URL + '/images/boipoka.png'} alt="" className='rounded-lg'/>
          <h2 className="text-sm font-medium text-left mt-2">BoiPoka</h2>
          <a href="https://boi-poka.vercel.app/"><h2 className='text-left text-xs text-gray-500 mt-1 '>Demo -></h2></a>
         </div>
 
         <div className="w-auto md:w-72 rounded-lg overflow-hidden shadow-lg px-4 py-4 border">
-         <img src={process.env.PUBLIC_URL + '/images/kontaNiben.png'} alt="" className='rounded-lg'/>
+         <img src={process.env.PUBLIC_URL + '/images/cineflix1.png'} alt="" className='rounded-lg'/>
          <h2 className="text-sm font-medium text-left mt-2">Cineflix</h2>
          <a href="https://cineflix-fiha.netlify.app/"><h2 className='text-left text-xs text-gray-500 mt-1 '>Demo -></h2></a>
         </div>
 
         <div className="w-auto md:w-72 rounded-lg overflow-hidden shadow-lg px-4 py-4 border">
-         <img src={process.env.PUBLIC_URL + '/images/kontaNiben.png'} alt="" className='rounded-lg'/>
+         <img src={process.env.PUBLIC_URL + '/images/kontaNiben.png'} alt="" className='rounded-lg w-full h-36'/>
          <h2 className="text-sm font-medium text-left mt-2">JU CSE Routinery</h2>
          <a href="https://github.com/nn-fiha/JU-CSE-ROUTINARY"><h2 className='text-left text-xs text-gray-500 mt-1 '>Git Link -></h2></a>
         </div>
