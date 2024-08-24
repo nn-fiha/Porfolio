1cd1793334d1b9da4c532309677b6dcd407ffaee
nn-fiha <jucse28.361@gmail.com>
2024-08-24 19:52:36 +0600

    modified project

--- src/porfolio/components/projects/Projects.jsx
+++ src/porfolio/components/projects/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
  
     <h2 className='text-2xl font-bold text-center ml-10 md:ml-0'>Projects</h2> 
     <span className='text-xs text-gray-500 text-center ml-10 md:ml-0'>My existing projects</span>
-    <div className="grid grid-cols-2 md:flex md:flex-wrap md:justify-center gap-5 md:gap-20 mt-10 md:px-20">
+    <div className="grid grid-cols-2 md:flex md:flex-wrap md:justify-center gap-3 md:gap-20 mt-10 md:px-20 ml-5 md:ml-0">
         <div className="w-auto md:w-72  rounded-lg shadow-lg px-4 py-4 border">
          <img src={process.env.PUBLIC_URL + '/images/OBE.png'} alt="" className='rounded-lg w-full h-36'/>
          <h2 className="text-sm font-medium text-left mt-2">OBE Based Result Management System</h2>
